bc6397970a78e07abbc282a99b8e1550dcb077a1
PhuruShekar <phurushotham@gmail.com>
2018-12-23 16:56:43 -0500

    more on album

--- albums/index.js
+++ albums/index.js
@@ -1,11 +1,15 @@
+/* eslint-disable react-native/no-inline-styles */
 //Import Library
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
-
+import { View,AppRegistry } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 //Make component
 const App = () => (
+    <View style={{ flex: 1 }}>
     <Header headerText={'Albums'} />
+    <AlbumList />
+    </View>
 );
 
 //Render it
